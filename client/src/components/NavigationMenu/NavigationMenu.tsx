bfc98787fc7b0928a0a43d0b77caab1703a4cfda
Vasilievkosta <vasilievkosta@gmail.com>
2025-10-01 22:22:28 +0300

    chore: changed readme.md

--- client/src/components/NavigationMenu/NavigationMenu.tsx
+++ client/src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,8 +1,10 @@
 import { NavLink } from "react-router-dom"
+import photo from "../../assets/avaGirl.jpg"
 
 export const NavigationMenu = () => {
   return (
     <div className="sidebar">
+      <img style={{ width: "100px" }} src={photo} />
       <NavLink to="/orders" className={({ isActive }) => (isActive ? "active" : "")}>
         Orders
       </NavLink>
