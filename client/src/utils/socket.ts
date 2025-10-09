531a77f62ae756932fac9a1fcadf41cf1264521e
Vasilievkosta <vasilievkosta@gmail.com>
2025-10-10 01:11:37 +0300

    chore: changed Dockerfile.dev

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,2 +1,4 @@
 import { io } from "socket.io-client"
-export const socket = io(import.meta.env.VITE_API_URL)
+export const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket"],
+})
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { visualizer } from "rollup-plugin-visualizer"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      filename: "stats.html",
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
