ea45da598a1dccb500bcf69a55ccbf1e072f3076
Vasilievkosta <vasilievkosta@gmail.com>
2025-10-10 11:24:53 +0300

    chore: changed vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,21 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // важно в Docker, чтобы слушал на 0.0.0.0
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://server:3000", // имя сервиса из docker-compose
+        changeOrigin: true,
+      },
+      "/socket.io": {
+        target: "http://server:3000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     rollupOptions: {
       output: {
